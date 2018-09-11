baa1f297475830fe82b1c7a50c5cb01e112029e8
Joshua Hunsche Jones <joshua@hunschejones.com>
2018-09-10 18:33:32 -0700

    Initial commit files for demo videos

---
+++ Node-Demo-App/Part 1/Video 2 Start/app.js
@@ -0,0 +1,33 @@
+const restify = require('restify')
+const logger  = require('morgan')
+const server = restify.createServer({
+  name: "NRQLjokes",
+  version: 1.0
+})
+server.use(logger('dev'))
+
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./db/sample.db')
+
+server.listen(process.env.PORT || 3000, () => {
+  server.get('/api/v1', (req, res, next) => {
+    var data = { 
+      "jokes": []
+    }
+
+    db.all('SELECT * FROM NRQLjokes', [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      rows.forEach((row) => {
+        data.jokes.push({
+          "id" : row.id,
+          "joke" : row.joke,
+          "punchline" : row.punchline
+        })
+      });
+      res.send(200, data);
+    });
+    return next();
+  })
+})
