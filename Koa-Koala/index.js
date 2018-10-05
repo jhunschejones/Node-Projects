0dc398ffd4a4a46d19449a925bdc4d0161214630
Joshua Hunsche Jones <joshua@hunschejones.com>
2018-10-05 13:46:31 -0700

    Add Cluster and workers

--- Koa-Koala/index.js
+++ Koa-Koala/index.js
@@ -1,57 +1,79 @@
 'use strict'
 const newrelic = require('newrelic')
-const koa = require('koa')
-const path = require('path')
-const render = require('koa-ejs')
-const koarouter = require('koa-router')
-const morgan = require('koa-morgan')
-const serve = require('koa-static')
+const cluster = require('cluster')
 
-const app = new koa()
-const router = new koarouter()
-const PORT = 3000
-// request logging
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
-// serving up the public folder so we can get to it from our index.html
-app.use(serve('./public'))
+if (cluster.isMaster) {
+  // count the machine's CPU's
+  const cpuCount = require('os').cpus().length
 
-render(app, {
-  root: path.join(__dirname, 'views'),
-  layout: 'index',
-  viewExt: 'html',
-  cache: false,
-  debug: false
-})
+  // create a worker for each CPU
+  for (var i = 0; i < cpuCount; i += 1) {
+    cluster.fork()
+  }
 
-router.get('koala', '/', (ctx) => {
-  let koala_attributes = []
-  koala_attributes.push({
-    meta_name: 'Color',
-    meta_value: 'Grey and white'
-  })
-  koala_attributes.push({
-    meta_name: 'Native Country',
-    meta_value: 'Austrailia'
-  })
-  koala_attributes.push({
-    meta_name: 'Animal Classification',
-    meta_value: 'Mammal'
-  })
-  koala_attributes.push({
-    meta_name: 'Life Span',
-    meta_value: '13 - 18 years'
-  })
-  koala_attributes.push({
-    meta_name: 'Are they bears',
-    meta_value: 'No!'
-  })
-  return ctx.render('index', {
-    attributes: koala_attributes,
-    image_path: "../images/koala_friends.png"
-  })
-})
+  // listen for dying workers
+  cluster.on('exit', function (worker) {
+    console.log("Worker %d died :(", worker.id)
 
-app.use(router.routes())
-app.use(router.allowedMethods())
+    // replace the dead worker
+    cluster.fork()
+  }) 
+} else {
+  // this code runs in a worker process
+  const koa = require('koa')
+  const path = require('path')
+  const render = require('koa-ejs')
+  const koarouter = require('koa-router')
+  const morgan = require('koa-morgan')
+  const serve = require('koa-static')
 
-app.listen(PORT, () => console.log(`Koalas are running on port: ${PORT}`))
+  const app = new koa()
+  const router = new koarouter()
+  const PORT = 3000
+  // request logging
+  app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+  // serving up the public folder so we can get to it from our index.html
+  app.use(serve('./public'))
+
+  render(app, {
+    root: path.join(__dirname, 'views'),
+    layout: 'index',
+    viewExt: 'html',
+    cache: false,
+    debug: false
+  })
+
+  router.get('koala', '/', (ctx) => {
+    let koala_attributes = []
+    koala_attributes.push({
+      meta_name: 'Color',
+      meta_value: 'Grey and white'
+    })
+    koala_attributes.push({
+      meta_name: 'Native Country',
+      meta_value: 'Austrailia'
+    })
+    koala_attributes.push({
+      meta_name: 'Animal Classification',
+      meta_value: 'Mammal'
+    })
+    koala_attributes.push({
+      meta_name: 'Life Span',
+      meta_value: '13 - 18 years'
+    })
+    koala_attributes.push({
+      meta_name: 'Are they bears',
+      meta_value: 'No!'
+    })
+    return ctx.render('index', {
+      attributes: koala_attributes,
+      image_path: "../images/koala_friends.png",
+      worker: cluster.worker.id
+    })
+  })
+
+  app.use(router.routes())
+  app.use(router.allowedMethods())
+
+  app.listen(PORT, () => console.log(`Koala worker ${cluster.worker.id} is running on port: ${PORT}`))
+}
